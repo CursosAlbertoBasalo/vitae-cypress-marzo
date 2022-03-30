9eacb6bf8a5b8155dbfaf180d402d4166451ee07
Alberto Basalo <albertobasalo@hotmail.com>
2022-03-30 16:26:42 +0200

    feat: samples 2 selector

--- cypress/integration/1-selectors/a2-selector.spec.ts
+++ cypress/integration/1-selectors/a2-selector.spec.ts
@@ -1,16 +1,19 @@
 describe("The budget home page", () => {
   // arrange
   const homePageUrl = "https://angularbuilders.github.io/angular-budget/";
+
   before(() => {
     // arrange
     cy.log("visit the page only once before all the tests");
     cy.visit(homePageUrl);
   });
+
   beforeEach(() => {
     // arrange
     cy.log("visit the page once before each the tests");
     //cy.visit(homePageUrl);
   });
+
   it("should display the application name", () => {
     cy.contains("Budget");
   });
@@ -35,6 +38,26 @@ describe("The budget home page", () => {
   it("should display the app name on something acting as banner", () => {
     // A selector based on a useful attribute: role, name, placeholder etc.
     cy.get("[role=banner]").should("contain", "Budget");
+  });
+
+  it("should have the app name on the title", () => {
+    cy.title().should("contain", "Budget");
+  });
+
+  it("should have link to a login url", () => {
+    // Check existence by complex selectors
+    cy.get('a[href*="login"]').should("exist");
+  });
+
+  it("should not have a link to a profile url", () => {
+    // check something does not exists
+    cy.get('a[href*="profile"]', { timeout: 5000 }).should("not.exist");
+  });
+
+  it("should not have orphaned links", { defaultCommandTimeout: 5000 }, () => {
+    // check several elements
+    cy.get("a").should("not.have.attr", "href", "#undefined");
+    cy.get("a").each(a => expect(a).to.not.have.attr("href", "undefined"));
   });
 
   afterEach(() => {
