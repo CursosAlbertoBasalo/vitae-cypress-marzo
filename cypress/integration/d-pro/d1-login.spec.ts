d6dadc4a6839334a266d0ff71dac854a4c0ec0e2
Alberto Basalo <albertobasalo@hotmail.com>
2022-04-06 18:12:34 +0200

    feat: login

--- cypress/integration/d-pro/d1-login.spec.ts
+++ cypress/integration/d-pro/d1-login.spec.ts
@@ -5,6 +5,8 @@ import { ProjectsAddPage } from "../../support/pages/projects-add.page";
 describe("GIVEN an authenticated user visiting the _projectsAdd_ page ", () => {
   const loginPage = new LoginPage();
   const projectsAddPage = new ProjectsAddPage();
+  let newId = "";
+  let authorization = "";
   before(() => {
     const url = Cypress.env("apiUrl") + "projects";
     loginPage.visit();
@@ -17,22 +19,27 @@ describe("GIVEN an authenticated user visiting the _projectsAdd_ page ", () => {
   context("WHEN submits a new project", () => {
     before(() => {
       projectsAddPage.fillAndSubmitCorrectlyFromFixture();
-      // ToDo: wait one
+      cy.wait("@postProject").then(call => {
+        authorization = call.request.headers["authorization"] as string;
+        newId = call.response?.body?.data?.id;
+      });
     });
     it("THEN should send auth token", () => {
-      cy.wait("@postProject").then(call => {
-        const requestAuth = call.request.headers["authorization"] as string;
-        expect(requestAuth).contains("Bearer ");
-      });
+      expect(authorization).contains("Bearer ");
     });
     it("AND THEN should got the create response", () => {
-      cy.wait("@postProject").then(call => {
-        const newId = call.response?.body?.data?.id;
-        expect(newId).not.be.empty;
-      });
+      expect(newId).not.be.empty;
     });
   });
   after(() => {
-    // ToDo: delete created project
+    cy.request({
+      method: "DELETE",
+      url: url + "/" + newId,
+      headers: {
+        authorization,
+      },
+    }).then(() => {
+      cy.visit("/");
+    });
   });
 });
