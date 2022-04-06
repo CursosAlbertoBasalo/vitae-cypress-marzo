6411673b44f0d5a762a2c82d6ae52644b1d34dcb
Alberto Basalo <albertobasalo@hotmail.com>
2022-04-06 10:29:24 +0200

    chre: refactor

--- cypress/integration/c-interceptors/c2-post-interceptor.spec.ts
+++ cypress/integration/c-interceptors/c2-post-interceptor.spec.ts
@@ -2,28 +2,33 @@
 
 import { ProjectsAddPage } from "../../support/pages/projects-add.page";
 
-describe("GIVEN the _Add new project_ page, connected to a teapot ", () => {
+describe("GIVEN the _Add new project_ page, connected to a teapot", () => {
   const projectsAddPage = new ProjectsAddPage();
   beforeEach(() => {
-    // arrange
-    const url = Cypress.env("apiUrl") + "projects/";
-    const response = { statusCode: 418, body: { data: [] } };
-    cy.intercept("POST", url, response).as("postProject");
+    // Arrange
+    const url = Cypress.env("apiUrl") + "projects";
+    // intercept of post request and responds with an error
+    const stubbedResponse = {
+      statusCode: 418, // I´m a teapot
+      body: { data: {} },
+    };
+    cy.intercept("POST", url, stubbedResponse).as("postProject"); // alias for the request
     projectsAddPage.visit();
   });
-
-  context("WHEN fill the form and submit to serve ", () => {
+  context("WHEN fill the form and submit to server", () => {
     beforeEach(() => {
-      // act
+      // Act
       projectsAddPage.fillCorrectlyFromFixture();
       projectsAddPage.submitButton.click();
     });
-    it("THEN correct data should be posted as a payload", () => {
-      // assert
-      cy.wait("@postProject").then(interceptedCall => {
-        const payload = interceptedCall.request.body;
-        expect(payload.name).to.eq("Collapse global maritime traffic");
-      });
+    it("THEN correct data should be posted as a payload ", () => {
+      // Assert
+      cy.wait("@postProject") // wait for the request to be done and assert with a function
+        .then(interceptedCall => {
+          // check that the project was posted as payload
+          const payload = interceptedCall.request.body;
+          expect(payload.name).to.eq("Collapse global maritime traffic");
+        });
     });
   });
 });
